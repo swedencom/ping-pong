01a38817ea8e5324787da506a8bfb69d84b0d503
swedencom <83535571+swedencom@users.noreply.github.com>
2021-05-23 15:05:54 +0500

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -51,11 +51,22 @@ background = transform.scale(image.load('fone.png'), (700,500))
 player = Player('playeer.png', 10, 50, 20, 150, 5)
 playert = Player('playeer.png', 670, 50, 20, 150, 5)
 
+ball = GameSprite('ball.png', 40, 20, 50, 50, 3)
 
 clock = time.Clock()
 FPS = 60
 game = True
 finish = False
+
+speed_y = 3
+speed_x = 3
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
 
 
 while game:
@@ -65,11 +76,30 @@ while game:
     if not finish:
         window.blit(background,(0,0))
 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
         player.update()
         playert.updatet()
 
+        ball.reset()
         player.reset()
         playert.reset()
+
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
+
+    if sprite.collide_rect(player, ball) or sprite.collide_rect(playert, ball):
+        speed_x *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (250,250))
+
+    if ball.rect.x > 650:
+        finish = True
+        window.blit(lose2, (250,250))
+
 
     display.update()
     clock.tick(FPS)
